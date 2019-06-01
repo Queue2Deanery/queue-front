8fafb1c200fd28325f9669b76c2107ec5e6adaa2
PAwelZ97 <pawelzych97@gmail.com>
2019-06-01 22:16:39 +0200

    MainPanel refactor and edit

--- src/components/prac/MainPanel.js
+++ src/components/prac/MainPanel.js
@@ -11,6 +11,14 @@ import TableBody from "@material-ui/core/TableBody";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import {Paper} from "@material-ui/core";
 
+const paperStyle = {
+  "padding":"15px",
+};
+
+const nextButtonStyle = {
+    "padding":"30px",
+    "min-width":"100%",
+};
 
 const useStyles = makeStyles(theme => ({
     '@global': {
@@ -29,8 +37,89 @@ const tier =
         queueList: ['3min', '7min', '8min', '8min']
     };
 
-function MainPanel() {
+function UserData() {
+    return (
+        <Paper style={paperStyle}>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={4}>
+                    Numer indeksu:
+                </Grid>
+                <Grid item xs={12} sm={8}>
+                    284777
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    Typ sprawy:
+                </Grid>
+                <Grid item xs={12} sm={8}>
+                    Przedłużenie legitymacji studenckiej
+                </Grid>
+            </Grid>
+        </Paper>
+    );
+}
+
+function QueueControl() {
+    return (
+        <Paper style={paperStyle}>
+        <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+                Liczba oczekujących:
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                11
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                Łączny czas
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                43 min
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Button>Wstrzymaj</Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Button>Zatrzymaj zapisy</Button>
+            </Grid>
+        </Grid>
+    </Paper>);
+}
+
+function QueueTable() {
     const classes = useStyles();
+    return (
+        <Paper style={paperStyle}>
+            <Table className={classes.table}>
+                <colgroup>
+                    <col style={{width: '10%'}}/>
+                    <col style={{width: '15%'}}/>
+                    <col style={{width: '15%'}}/>
+                    <col style={{width: '60%'}}/>
+                </colgroup>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Pozycja</TableCell>
+                        <TableCell>Numer indeksu</TableCell>
+                        <TableCell>Przewidywany czas</TableCell>
+                        <TableCell>Typ sprawy</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {tier.queueList.map((line, index) => (
+                        <TableRow key={index}>
+                            <TableCell>{index}</TableCell>
+                            <TableCell>{123456}</TableCell>
+                            <TableCell>{line}</TableCell>
+                            <TableCell>Lorem ipsum w kuri tiknu i fsjdu adko. kosla</TableCell>
+                        </TableRow>
+                    ))}
+
+                </TableBody>
+            </Table>
+        </Paper>
+    );
+}
+
+function MainPanel() {
     return (
         <React.Fragment>
             <AppBar/>
@@ -38,90 +127,23 @@ function MainPanel() {
                 <h3>Infomatyka</h3>
                 <Grid container spacing={5}>
                     <Grid item xs={12}>
-                        <Paper>
-                            <Grid container spacing={3}>
-                                <Grid item xs={12} md={4}>
-                                    Imię i nazwisko:
-                                </Grid>
-                                <Grid item xs={12} md={8}>
-                                    Jan Kowalski
-                                </Grid>
-                                <Grid item xs={12} md={4}>
-                                    Numer indeksu:
-                                </Grid>
-                                <Grid item xs={12} md={8}>
-                                    284777
-                                </Grid>
-                                <Grid item xs={12} md={4}>
-                                    Typ sprawy:
-                                </Grid>
-                                <Grid item xs={12} md={8}>
-                                    Przedłużenie legitymacji studenckiej
-                                </Grid>
-                            </Grid>
-                        </Paper>
+                        <UserData/>
                     </Grid>
-                    <Grid item xs={12} md={8}>
-                        <Paper>
-                            <Grid container spacing={3}>
-                                <Grid item xs={12} md={6}>
-                                    Liczba oczekujących:
-                                </Grid>
-                                <Grid item xs={12} md={6}>
-                                    11
-                                </Grid>
-                                <Grid item xs={12} md={6}>
-                                    Łączny czas
-                                </Grid>
-                                <Grid item xs={12} md={6}>
-                                    43 min
-                                </Grid>
-                                <Grid item xs={12} md={6}>
-                                    <Button>Wstrzymaj</Button>
-                                </Grid>
-                                <Grid item xs={12} md={6}>
-                                    <Button>Zatrzymaj zapisy</Button>
-                                </Grid>
-                            </Grid>
-                        </Paper>
+                    <Grid item xs={12} sm={8}>
+                       <QueueControl/>
                     </Grid>
-                    <Grid item xs={12} md={4}>
+                    <Grid item xs={12} sm={4}>
                         <Grid container spacing={3}>
                             <Grid item xs={12}>
-                                <Button>Poprzedni</Button>
+                                <Button color="primary" variant="contained">Poprzedni</Button>
                             </Grid>
                             <Grid item xs={12}>
-                                <Button>Następny</Button>
+                                <Button color="primary" variant="contained" size={"large"} style={nextButtonStyle}>Następny</Button>
                             </Grid>
                         </Grid>
                     </Grid>
                     <Grid item xs={12}>
-                        <Paper>
-                            <Table className={classes.table}>
-                                <colgroup>
-                                    <col style={{width: '20%'}}/>
-                                    <col style={{width: '40%'}}/>
-                                    <col style={{width: '40%'}}/>
-                                </colgroup>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Pozycja</TableCell>
-                                        <TableCell>Numer indeksu</TableCell>
-                                        <TableCell>Przewidywany czas</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {tier.queueList.map((line, index) => (
-                                        <TableRow key={index}>
-                                            <TableCell>{index}</TableCell>
-                                            <TableCell>{123456}</TableCell>
-                                            <TableCell>{line}</TableCell>
-                                        </TableRow>
-                                    ))}
-
-                                </TableBody>
-                            </Table>
-                        </Paper>
+                       <QueueTable/>
                     </Grid>
                 </Grid>
             </Container>
